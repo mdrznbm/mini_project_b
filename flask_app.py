739ccd55b55341b7e5cb448676f25d3e39a37238
Mohammad Shahrazin <mdrznbm.w@gmail.com>
2026-08-06 07:04:34 +0000

    Yay, it's all finished :-D

--- flask_app.py
+++ flask_app.py
@@ -11,6 +11,7 @@ SQLALCHEMY_DATABASE_URI = "sqlite:////home/mdrznbm/mysite/comments.db"
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
 
 class Comment(db.Model):
@@ -20,17 +21,14 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
-#comments = []
-
 @app.route("/", methods=["GET", "POST"])
 
 def index():
     if request.method == "GET":
-#       return render_template("main_page.html", comments=comments)
         return render_template("main_page.html", comments=Comment.query.all())
-#   comments.append(request.form["contents"])
-#   return redirect(url_for('index'))
+
     comment = Comment(content=request.form["contents"])
     db.session.add(comment)
     db.session.commit()
+    return redirect(url_for('index'))
 
