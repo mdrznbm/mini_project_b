4caf6c5ed0bc5c061a4ef7dce70cfc0214a6a654
Mohammad Shahrazin <mdrznbm.w@gmail.com>
2026-08-07 02:40:00 +0000

    settled login / logout page

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,9 @@
-# A very simple Flask Hello World app for you to get started with...
+# A very simple Flask app for you to get started with...
 
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user, logout_user, login_required, LoginManager, UserMixin, current_user
+from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
@@ -13,7 +15,34 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+app.secret_key = "mini project a and b"
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+# --- User Model ---
+class User(UserMixin):
+    def __init__(self, username, password_hash):
+        self.username = username
+        self.password_hash = password_hash
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return self.username
+
+# --- User Database / Mapping ---
+all_users = {
+    "admin": User("admin", generate_password_hash("secret")),
+    "bob": User("bob", generate_password_hash("less-secret")),
+    "caroline": User("caroline", generate_password_hash("completely-secret")),
+}
+
+@login_manager.user_loader
+def load_user(user_id):
+    return all_users.get(user_id)
+
+# --- Comment Database Model ---
 class Comment(db.Model):
 
     __tablename__ = "comments"
@@ -21,12 +50,19 @@ class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
 
+# --- Routes ---
 @app.route("/", methods=["GET", "POST"])
 
 def index():
+    # If it's a GET request, show the page and all comments
     if request.method == "GET":
         return render_template("main_page.html", comments=Comment.query.all())
 
+    # If an unauthenticated user tries to submit a POST request, redirect them
+    if not current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    # If they are logged in, save the comment
     comment = Comment(content=request.form["contents"])
     db.session.add(comment)
     db.session.commit()
@@ -37,8 +73,32 @@ def login():
     if request.method == "GET":
         return render_template("login_page.html", error=False)
 
-    if request.form["username"] != "admin" or request.form["password"] != "secret":
+    username = request.form["username"]
+    if username not in all_users:
+        return render_template("login_page.html", error=True)
+    user = all_users[username]
+
+    if not user.check_password(request.form["password"]):
         return render_template("login_page.html", error=True)
 
+    login_user(user)
     return redirect(url_for('index'))
+
+@app.route("/logout/")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+
+
+
+
+
+
+
+
+
 
